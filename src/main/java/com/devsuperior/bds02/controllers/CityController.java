61d3a8478ab2456ff34dcc79915d60f766d70c64
Rafael V. dos Santos <rvsantos00@gmail.com>
2022-03-22 20:35:13 -0300

    test: findAllShouldReturnAllResourcesSortedByName

---
+++ src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.bds02.controllers;
+
+import java.util.List;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.services.CityService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cities")
+public class CityController {
+    @Autowired
+    private CityService service;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findAll());
+    }
+}
---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.bds02.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.repositories.CityRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CityService {
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        return repository.findAll(Sort.by("name"))
+                .stream()
+                .map(CityDTO::new)
+                .collect(Collectors.toList());
+    }
+}
